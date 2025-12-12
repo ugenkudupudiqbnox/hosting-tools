e52ca0e1b2dfcde95faee168f0abd55e1f2c153c
ugenkudupudiqbnox <ugen@qbnox.com>
2025-12-13 00:31:56 +0530

    Add script to update Pressbooks dependencies

---
+++ updatepb.sh
@@ -0,0 +1,3 @@
+sudo -u www-data composer update pressbooks/pressbooks --with-dependencies
+sudo -u www-data composer update pressbooks/pressbooks-aldine --with-dependencies
+sudo -u www-data composer update pressbooks/pressbooks-book --with-dependencies
